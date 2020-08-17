539fef635425192c327e7e21f36d43f9ef5f990f
Lucas <lucas.porto@aisdigital.com.br>
2020-08-16 23:07:12 -0300

    :bug: fix github env var heroku deploy

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,12 @@
 export const environment = {
   production: false,
   URL_API: 'https://api.github.com/users',
-  GITHUB_ACCESS_TOKEN: '70910749b585cfee81fe3f55d7b0e7f8de3d8f93'
+  GTB_ACCSS_TKN: (): string => {
+    const gtbAccssTknInitial = '3b43614cab7e4f133..gtbAccssTkn..263a89f54cc7a09d6d3543a';
+    const part1 = gtbAccssTknInitial.split('..gtbAccssTkn..')[0];
+    const part2 = gtbAccssTknInitial.split('..gtbAccssTkn..')[1];
+    return part1.concat(part2);
+  }
 };
 
 /*
