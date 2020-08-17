40473c307a1fa4b6029746210abbf7bd871dcad2
Lucas <lucas.porto@aisdigital.com.br>
2020-08-16 21:02:50 -0300

    :sparkles: fix toast and Implement deploy on Heroku

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   URL_API: 'https://api.github.com/users',
-  GITHUB_ACCESS_TOKEN: 'fbfd4e4191f366d646fbc74c1df3c8dc7319b187'
+  GITHUB_ACCESS_TOKEN: '70910749b585cfee81fe3f55d7b0e7f8de3d8f93'
 };
 
 /*
